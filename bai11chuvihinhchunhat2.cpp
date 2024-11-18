00fd8fccb78f2a3e66a3fc98aa66f5814eeb1ad3
anhdatsiudeptrai <ddat5441@gmail.com>
2024-11-18 09:15:01 +0700

    Add project files.

---
+++ bai11chuvihinhchunhat2.cpp
@@ -0,0 +1,20 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "tinh dien tich chu vi hinh tron\n";
+	double chuvi, dientich, bankinh;
+
+	cout << "nhap bankinh "; cin >> bankinh;
+	//SỬ DỤNG M_PI THAY VÌ SỬ DỤNG PI= 3.14
+
+	chuvi = 2 * M_PI * bankinh;
+	dientich = M_PI * bankinh * bankinh;
+
+	cout << "chuvi :" << chuvi << endl;
+	cout << "dientich :" << dientich << endl;
+	return 0;
+
+
+}
